5b64eea09d7a3cba044ed59854a4d6ba2d0cf20d
Ivan Koryakovskiy <i.koryakovskiy@tudelft.nl>
2017-10-31 16:23:57 +0100

    Prepared GPU version to run on server

--- single_ddpg.py
+++ single_ddpg.py
@@ -9,7 +9,7 @@ global_params.init()
 
 #global_params.ou_sigma = 0.1
 #global_params.ou_theta = 0.15
-d = {'transitions': {'load': 0, 'save': 1, 'save_filename': 'data_train_ddpg', 'buffer_size': 5000},
+d = {'transitions': {'load': 0, 'save': 1, 'save_filename': 'db_trajectories', 'buffer_size': 5000},
      'difference_model': 0}
 with open('config.yaml', 'w') as yaml_file:
     yaml.dump(d, yaml_file, default_flow_style=False)
--- single_ddpg_play.py
+++ single_ddpg_play.py
@@ -7,8 +7,8 @@ from main_ddpg import start
 
 global_params.init()
 
-d = {'transitions': {'load': 1, 'load_filename': 'data_train_ddpg', 'save': 1,
-                       'save_filename': 'data_train_ddpg_play', 'buffer_size': 5000},
+d = {'transitions': {'load': 1, 'load_filename': 'db_trajectories', 'save': 1,
+                       'save_filename': 'db_trajectories_play', 'buffer_size': 5000},
      'difference_model': 0}
 with open('config.yaml', 'w') as yaml_file:
     yaml.dump(d, yaml_file, default_flow_style=False)
