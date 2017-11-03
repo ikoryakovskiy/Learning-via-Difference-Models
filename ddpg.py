4ceddc47e8f7ca5d528d8768d1d8b3e7fb04603d
Ivan Koryakovskiy <i.koryakovskiy@tudelft.nl>
2017-11-03 22:36:14 +0100

    Adding yaml file as argument

--- ddpg.py
+++ ddpg.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import yaml, collections
+import argparse
 import os
 import ddpg_params
 from main_ddpg import start
@@ -17,26 +18,29 @@ def dict_constructor(loader, node):
 
 ######################################################################################
 if __name__ == "__main__":
-    cfg = "../grl/qt-build/cfg/leo/drl/rbdl_ddpg.yaml"
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('cfg', nargs='?', default="../grl/qt-build/cfg/leo/drl/rbdl_ddpg.yaml")
+    args = parser.parse_args()
 
     # create a copy of cfg with DDPG learning parameters in temporary location
     loc = "tmp"
     if not os.path.exists(loc):
         os.makedirs(loc)
-    new_cfg = "{}/{}".format(loc, os.path.basename(cfg))
+    new_cfg = "{}/{}".format(loc, os.path.basename(args.cfg))
 
     # for walking with yaml files
     _mapping_tag = yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG
     yaml.add_representer(collections.OrderedDict, dict_representer)
     yaml.add_constructor(_mapping_tag, dict_constructor)
 
-    with open(cfg, 'r') as f:
+    # append ddpg_param if missing
+    with open(args.cfg, 'r') as f:
         conf = yaml.load(f)
-
-    conf['ddpg_param'] = ddpg_params.init()
-
-    with open(new_cfg, 'w') as f:
-        yaml.dump(conf, f)
+        if not "ddpg_param" in conf:
+            conf['ddpg_param'] = ddpg_params.init()
+            with open(new_cfg, 'w') as f:
+                yaml.dump(conf, f)
 
     # Use newly created configuration
     start(new_cfg)
