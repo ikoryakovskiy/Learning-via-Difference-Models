058c8232ffea07495f6ec15938e0bf1095c71e3a
Ivan Koryakovskiy <i.koryakovskiy@tudelft.nl>
2018-03-22 09:26:56 +0100

    Fixing a wrong argument naming

--- ddpg.py
+++ ddpg.py
@@ -66,7 +66,7 @@ def parse_args():
     parser.add_argument('--env-report', type=str, default='test')
     parser.add_argument('--trials', type=int, default=0)
     parser.add_argument('--steps', type=int, default=1000) #
-    parser.add_argument('--reach_duration', type=float, default=0, help='Finish if trial happend to be longer then reach_balance twice in a row. 0 means desabled')
+    parser.add_argument('--reach_timeout', type=float, default=0, help='Finish if trial happend to be longer then reach_balance twice in a row. 0 means desabled')
     parser.add_argument('--reach-return', type=float, default=None)
     parser.add_argument('--default-damage', type=float, default=None)
     parser.add_argument('--test-interval', type=int, default=30)
