4370ec6472b7a67c1044cc51c5884f1e7c9eba93
Ivan Koryakovskiy <i.koryakovskiy@tudelft.nl>
2018-03-22 09:22:18 +0100

    Reduing rnn-based trainging up to 300000 steps

--- mp_new_curriculum.py
+++ mp_new_curriculum.py
@@ -20,21 +20,21 @@ def main():
     runs = range(16)
 
     mp_cfgs = []
-#    nn_params=("long_curriculum_network", "long_curriculum_network_stat.pkl")
-#    mp_cfgs += do_network_based(args, cores, name='ddpg-cl_long', nn_params=nn_params, runs=runs)
-#
-#    nn_params=("short_curriculum_network", "short_curriculum_network_stat.pkl")
-#    mp_cfgs += do_network_based(args, cores, name='ddpg-cl_short', nn_params=nn_params, runs=runs)
+    nn_params=("long_curriculum_network", "long_curriculum_network_stat.pkl")
+    mp_cfgs += do_network_based(args, cores, name='ddpg-cl_long', nn_params=nn_params, runs=runs)
+
+    nn_params=("short_curriculum_network", "short_curriculum_network_stat.pkl")
+    mp_cfgs += do_network_based(args, cores, name='ddpg-cl_short', nn_params=nn_params, runs=runs)
 
 #    mp_cfgs += do_steps_based(args, cores, name='ddpg-bbw', steps=(20000, 30000, 250000), runs=runs)
 #    mp_cfgs += do_steps_based(args, cores, name='ddpg-bw',  steps=(   -1, 50000, 250000), runs=runs)
 #    mp_cfgs += do_steps_based(args, cores, name='ddpg-w',   steps=(   -1,    -1, 300000), runs=runs)
 
     # naive switching after achieving the balancing for n number of seconds happening twice. 0 means not used
-    mp_cfgs += do_reach_timeout_based(args, cores, name='ddpg-rb55', reach_timeout=(5.0, 5.0, 0.0), runs=runs)
-    mp_cfgs += do_reach_timeout_based(args, cores, name='ddpg-rb5', reach_timeout=(-1.0, 5.0, 0.0), runs=runs)
-    mp_cfgs += do_reach_timeout_based(args, cores, name='ddpg-rb2020', reach_timeout=(20.0, 20.0, 0.0), runs=runs)
-    mp_cfgs += do_reach_timeout_based(args, cores, name='ddpg-rb2020', reach_timeout=(-1.0, 20.0, 0.0), runs=runs)
+#    mp_cfgs += do_reach_timeout_based(args, cores, name='ddpg-rb55', reach_timeout=(5.0, 5.0, 0.0), runs=runs)
+#    mp_cfgs += do_reach_timeout_based(args, cores, name='ddpg-rb5', reach_timeout=(-1.0, 5.0, 0.0), runs=runs)
+#    mp_cfgs += do_reach_timeout_based(args, cores, name='ddpg-rb2020', reach_timeout=(20.0, 20.0, 0.0), runs=runs)
+#    mp_cfgs += do_reach_timeout_based(args, cores, name='ddpg-rb2020', reach_timeout=(-1.0, 20.0, 0.0), runs=runs)
 
     # DBG: export configuration
     export_cfg(mp_cfgs)
@@ -94,7 +94,6 @@ def do_reach_timeout_based(base_args, cores, name, reach_timeout, runs):
 def do_network_based(base_args, cores, name, nn_params, runs):
     args = base_args.copy()
     args['rb_min_size'] = 1000
-    args['reach_return'] = 1422.66
     args['default_damage'] = 4035.00
     args['perf_td_error'] = True
     args['perf_l2_reg'] = True
