Fixing two important bugs. 1) forward promotion was incorrectly added to the replay buffer (correct: it is the last element of the state) 2) weighting coeffecient for forward promotion is [s/m], i.e. duration of a singe step is accounted in it

ddpg_loop.py
@@ -32,7 +32,7 @@ def preload_policy(sess, config):
             load_file = "{}/{}".format(path, load_file)
             meta_file = "{}.meta".format(load_file)
             if os.path.isfile(meta_file):
-                var_all = tf.all_variables() #tf.trainable_variables()
+                var_all = tf.global_variables() #tf.trainable_variables()
                 var_this = [v for v in var_all if not 'curriculum' in v.name]
                 saver = tf.train.Saver(var_this)
                 saver.restore(sess, load_file)
@@ -48,7 +48,7 @@ def preload_policy(sess, config):
 
 def save_policy(sess, config, suffix = "", global_step = None):
     if config["output"]:
-        var_all = tf.all_variables() #tf.trainable_variables()
+        var_all = tf.global_variables() #tf.trainable_variables()
         var_this = [v for v in var_all if not 'curriculum' in v.name]
         saver = tf.train.Saver(var_this)
         saver.save(sess, "./{}{}".format(config["output"], suffix), global_step)