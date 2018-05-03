fbea694ee8cf998632bcc529e1c7e0c32519e7a4
Ivan Koryakovskiy <i.koryakovskiy@tudelft.nl>
2018-05-03 17:42:33 +0200

    cl_stages was removed from defaults => make it explicit

--- mp_new_curriculum.py
+++ mp_new_curriculum.py
@@ -18,9 +18,9 @@ def main():
     print('Using {} cores.'.format(cores))
 
     # Parameters
-    runs = range(32)
-    model = 'perturbed'
-    #model = 'idealized'
+    runs = range(1)
+    #model = 'perturbed'
+    model = 'idealized'
 
     if model == 'idealized':
         tasks = {
@@ -39,22 +39,22 @@ def main():
     misc = {'tasks':tasks, 'starting_task':starting_task, 'runs':runs}
 
     mp_cfgs = []
-    nn_params=("long_curriculum_network", "long_curriculum_network_stat.pkl")
-    mp_cfgs += do_network_based(args, cores, name='ddpg-cl_long', nn_params=nn_params, **misc)
+#    nn_params=("long_curriculum_network", "long_curriculum_network_stat.pkl")
+#    mp_cfgs += do_network_based(args, cores, name='ddpg-cl_long', nn_params=nn_params, **misc)
 
     nn_params=("short_curriculum_network", "short_curriculum_network_stat.pkl")
     mp_cfgs += do_network_based(args, cores, name='ddpg-cl_short', nn_params=nn_params, **misc)
 
-    mp_cfgs += do_steps_based(args, cores, name='ddpg-bbw', steps=(20000, 30000, 250000), **misc)
-    mp_cfgs += do_steps_based(args, cores, name='ddpg-bw',  steps=(   -1, 50000, 250000), **misc)
-    mp_cfgs += do_steps_based(args, cores, name='ddpg-w',   steps=(   -1,    -1, 300000), **misc)
-
-    # naive switching after achieving the balancing for n number of seconds happening twice. 0 means not used
-    mp_cfgs += do_reach_timeout_based(args, cores, name='ddpg-rb55', reach_timeout=(5.0, 5.0, 0.0), **misc)
-    mp_cfgs += do_reach_timeout_based(args, cores, name='ddpg-rb5', reach_timeout=(-1.0, 5.0, 0.0), **misc)
-
-    mp_cfgs += do_reach_timeout_based(args, cores, name='ddpg-rb2020', reach_timeout=(20.0, 20.0, 0.0), **misc)
-    mp_cfgs += do_reach_timeout_based(args, cores, name='ddpg-rb20', reach_timeout=(-1.0, 20.0, 0.0), **misc)
+#    mp_cfgs += do_steps_based(args, cores, name='ddpg-bbw', steps=(20000, 30000, 250000), **misc)
+#    mp_cfgs += do_steps_based(args, cores, name='ddpg-bw',  steps=(   -1, 50000, 250000), **misc)
+#    mp_cfgs += do_steps_based(args, cores, name='ddpg-w',   steps=(   -1,    -1, 300000), **misc)
+#
+#    # naive switching after achieving the balancing for n number of seconds happening twice. 0 means not used
+#    mp_cfgs += do_reach_timeout_based(args, cores, name='ddpg-rb55', reach_timeout=(5.0, 5.0, 0.0), **misc)
+#    mp_cfgs += do_reach_timeout_based(args, cores, name='ddpg-rb5', reach_timeout=(-1.0, 5.0, 0.0), **misc)
+#
+#    mp_cfgs += do_reach_timeout_based(args, cores, name='ddpg-rb2020', reach_timeout=(20.0, 20.0, 0.0), **misc)
+#    mp_cfgs += do_reach_timeout_based(args, cores, name='ddpg-rb20', reach_timeout=(-1.0, 20.0, 0.0), **misc)
 
     # DBG: export configuration
     export_cfg(mp_cfgs)
@@ -63,9 +63,9 @@ def main():
     # Run all scripts at once
     random.shuffle(mp_cfgs)
     prepare_multiprocessing()
-    do_multiprocessing_pool(cores, mp_cfgs)
-    #config, tasks, starting_task = mp_cfgs[0]
-    #cl_run(tasks, starting_task, **config)
+    #do_multiprocessing_pool(cores, mp_cfgs)
+    config, tasks, starting_task = mp_cfgs[0]
+    cl_run(tasks, starting_task, **config)
 
 
 def do_steps_based(base_args, cores, name, steps, runs, tasks, starting_task):
@@ -106,6 +106,7 @@ def do_network_based(base_args, cores, name, nn_params, runs, tasks, starting_ta
     args['steps'] = 300000
     args["cl_batch_norm"] = False
     args['cl_structure'] = 'rnnc:gru_tanh_6_dropout;fc_linear_3'
+    args['cl_stages'] = 'balancing_tf;balancing;walking:monotonic'
     args['cl_depth'] = 2
     args['cl_pt_shape'] = (2,3)
     args["cl_pt_load"] = nn_params[1]
