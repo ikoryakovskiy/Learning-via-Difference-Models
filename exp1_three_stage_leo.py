9fb3401c0bbe44eaaa4eeb6a4c08c4012b7b1303
Ivan Koryakovskiy <i.koryakovskiy@tudelft.nl>
2018-07-01 07:30:02 +0200

    According to the power test, at least 37 repetioions are needed

--- exp1_three_stage_leo.py
+++ exp1_three_stage_leo.py
@@ -18,7 +18,7 @@ def main():
     print('Using {} cores.'.format(cores))
 
     # Parameters
-    runs = range(16, 32)
+    runs = range(16, 37)
     exp_name = "ddpg-exp1_three_stage"
 
     tasks = {
